a134bad01d4dfe5b23247dfb5498c4906a71ae0c
fzhbioinfo <fzhbioinfo@163.com>
2025-12-29 11:30:05 +0800

    style: :bento: 使用Enum枚举

--- deeplearning/spliceai/create_dataset.py
+++ deeplearning/spliceai/create_dataset.py
@@ -3,6 +3,7 @@ import numpy as np
 from argparse import ArgumentParser
 import h5py
 from itertools import count
+from enum import Enum
 
 
 # 序列长度
@@ -17,6 +18,13 @@ BASE = np.array([
     [0, 0, 0, 1]
 ], dtype=np.int8)
 
+class Base_Index(Enum):
+    N = '0'
+    A = '1'
+    C = '2'
+    G = '3'
+    T = '4'
+
 # Neither, Acceptor, Donor, Padding
 SPLICE = np.array([
     [1, 0, 0],
@@ -25,9 +33,15 @@ SPLICE = np.array([
     [0, 0, 0]
 ], dtype=np.int8)
 
+class Splice_Index(Enum):
+    NEITHER = 0
+    ACCEPTOR = 1
+    DONOR = 2
+    PADDING = -1
+
 
 def seq_to_numeric(seq):
-    seq = seq.upper().replace('A', '1').replace('C', '2').replace('G', '3').replace('T', '4').replace('N', '0')
+    seq = seq.upper().replace('A', Base_Index.A.value).replace('C', Base_Index.C.value).replace('G', Base_Index.G.value).replace('T', Base_Index.T.value).replace('N', Base_Index.N.value)
     return seq
 
 
@@ -60,22 +74,22 @@ def run(args):
         if row.strand == '+':
             for js in jn_start:
                 if row.tx_start <= js <= row.tx_end:
-                    splice[js - row.tx_start] = 2
+                    splice[js - row.tx_start] = Splice_Index.DONOR.value
             for je in jn_end:
                 if row.tx_start <= je <= row.tx_end:
-                    splice[je - row.tx_start] = 1
+                    splice[je - row.tx_start] = Splice_Index.ACCEPTOR.value
         elif row.strand == '-':
             for js in jn_start:
                 if row.tx_start <= js <= row.tx_end:
-                    splice[row.tx_end - js] = 1
+                    splice[row.tx_end - js] = Splice_Index.ACCEPTOR.value
             for je in jn_end:
                 if row.tx_start <= je <= row.tx_end:
-                    splice[row.tx_end - je] = 2
+                    splice[row.tx_end - je] = Splice_Index.DONOR.value
         else:
             raise Exception("strand error")
         # 序列和splice信息右侧填充5k，确保每个基因长度都够15k，后面再以15k长度，5k窗口滑动创建单元数据集
-        seq = np.pad(seq, (0, L), 'constant', constant_values=0)
-        splice = np.pad(splice, (0, L), 'constant', constant_values=-1)
+        seq = np.pad(seq, (0, L), 'constant', constant_values=int(Base_Index.N.value))
+        splice = np.pad(splice, (0, L), 'constant', constant_values=Splice_Index.PADDING.value)
         # 一个基因能构建的单元数据集数量
         nums = np.ceil((row.tx_end - row.tx_start + 1) / L)
         # 滑动创建单元数据集
